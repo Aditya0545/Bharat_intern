6be8296098a4978a050fdad1825b5a5694b18327
Aditya Kumar <105490754+Aditya0545@users.noreply.github.com>
2023-12-03 22:41:33 +0530

    updateall

--- TASK2/script.js
+++ TASK2/script.js
@@ -1,38 +1,59 @@
-// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = '2b97731d51e1d0715d41ee3722475308';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    const locationElement = document.getElementById('location');
+    const temperatureElement = document.getElementById('temperature');
+    const descriptionElement = document.getElementById('description');
+    const weatherIconElement = document.getElementById('weather-icon');
+    const cityInput = document.getElementById('cityInput');
+    const searchBtn = document.getElementById('searchBtn');
 
-// Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-const apiKey = '2b97731d51e1d0715d41ee3722475308';
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    function updateWeather(city) {
+        const url = `${apiUrl}${city}&appid=${apiKey}&units=metric`;
 
-function getWeather() {
-    // Get the user-entered city from the input field
-    const cityInput = document.getElementById('city');
-    const city = cityInput.value;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const { name, main, weather } = data;
+                locationElement.textContent = name;
+                temperatureElement.textContent = `${Math.round(main.temp)}°C`;
+                descriptionElement.textContent = weather[0].description;
 
-    // Check if the city is not empty
-    if (city.trim() === '') {
-        alert('Please enter a city');
-        return;
+                const weatherIconClass = getWeatherIconClass(weather[0].id);
+                weatherIconElement.className = `wi ${weatherIconClass}`;
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
     }
 
-    // Construct the API URL with the city and API key
-    const apiUrl = `${baseUrl}?q=${city}&appid=${apiKey}`;
+    function getWeatherIconClass(weatherId) {
+        if (weatherId >= 200 && weatherId < 300) {
+            return 'wi-thunderstorm';
+        } else if (weatherId >= 300 && weatherId < 500) {
+            return 'wi-showers';
+        } else if (weatherId >= 500 && weatherId < 600) {
+            return 'wi-rain';
+        } else if (weatherId >= 600 && weatherId < 700) {
+            return 'wi-snow';
+        } else if (weatherId >= 700 && weatherId < 800) {
+            return 'wi-fog';
+        } else if (weatherId === 800) {
+            return 'wi-day-sunny';
+        } else {
+            return 'wi-day-cloudy';
+        }
+    }
 
-    // Fetch weather data from the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Update the DOM with the weather information
-            const locationElement = document.getElementById('location');
-            const temperatureElement = document.getElementById('temperature');
-            const descriptionElement = document.getElementById('description');
+    searchBtn.addEventListener('click', function () {
+        const cityName = cityInput.value.trim();
+        if (cityName !== '') {
+            updateWeather(cityName);
+        } else {
+            alert('Please enter a city name.');
+        }
+    });
 
-            locationElement.textContent = `Weather in ${data.name}, ${data.sys.country}`;
-            temperatureElement.textContent = `Temperature: ${data.main.temp}°C`;
-            descriptionElement.textContent = `Description: ${data.weather[0].description}`;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            alert('Error fetching weather data. Please try again later.');
-        });
-}
+    // Example: Update weather for 'London' on page load
+    updateWeather('London');
+});
